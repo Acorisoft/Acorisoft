57296c7fab30a662df3d9e4f82c95b74f350882b
Acoris <731984870@qq.com>
2021-02-24 22:20:38 +0800

    添加基本灵感文档元素

---
+++ src/Acorisoft.Morisa.Documents/Morisa/Inspirations/IInspirationElement.cs
@@ -0,0 +1,20 @@
+﻿using Acorisoft.Morisa.Tags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.Morisa.Inspirations
+{
+    /// <summary>
+    /// <see cref="IInspirationElement"/> 定义了一个灵感元素的基本功能。
+    /// </summary>
+    /// <remarks>
+    /// <see cref="IInspirationElement"/> 灵感元素
+    /// </remarks>
+    public interface IInspirationElement
+    {
+        ITagCollection Tags { get; }
+    }
+}
---
+++ src/Acorisoft.Morisa.Documents/Morisa/Inspirations/IPictureElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.Morisa.Inspirations
+{
+    /// <summary>
+    /// <see cref="IPictureElement"/> 接口定义一个图片元素
+    /// </summary>
+    public interface IPictureElement : IInspirationElement
+    {
+    }
+}
