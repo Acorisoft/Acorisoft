2e0aaa76a774dbd45c787d3e65a9b564646709b0
Acoris <731984870@qq.com>
2021-03-02 10:11:42 +0800

    添加了画廊控件，并完成测试

--- src/Acorisoft.Morisa.UI/Properties/AssemblyInfo.cs
+++ src/Acorisoft.Morisa.UI/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Panels")]
-//[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Routers")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Controls")]
 //[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Dialogs")]
 
