6855006c4ec127649a4c35f3c76436ac6850d126
Acoris <731984870@qq.com>
2021-03-08 10:06:04 +0800

    重新规划App

--- src/Acorisoft.Morisa.UI/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows;
-using System.Windows.Markup;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Panels")]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Controls")]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Morisa.Controls.Buttons")]
-
