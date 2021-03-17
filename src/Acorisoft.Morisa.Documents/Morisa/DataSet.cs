49db95bb8b70a6dc9e65cbc349ecbee8e9e2872a
Acoris <731984870@qq.com>
2021-03-17 22:44:26 +0800

    添加数据集模式

---
+++ src/Acorisoft.Morisa.Documents/Morisa/DataSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.Morisa
+{
+    public abstract class DataSet
+    {
+        protected internal LiteDatabase Database { get; set; }
+    }
+}
---
+++ src/Acorisoft.Morisa.Documents/Morisa/DataSetManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.Morisa
+{
+    public abstract class DataSetManager<TDataSet> where TDataSet : DataSet
+    {
+
+    }
+}
