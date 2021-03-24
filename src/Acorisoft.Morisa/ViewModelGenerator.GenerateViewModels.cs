7ba3795e56c56a19521d36b760040cb986ae4bb3
luoyingliang <731984870@qq.com>
2021-03-24 15:29:41 +0800

    完成自动生成的T4模板

--- src/Acorisoft.Morisa/ViewModelGenerator.GenerateViewModels.cs
+++ src/Acorisoft.Morisa/ViewModelGenerator.GenerateViewModels.cs
@@ -1,1 +1,22 @@
-﻿ErrorDebuggingTemplate
+﻿
+
+
+using Acorisoft.Morisa.ViewModels;
+using Acorisoft.Morisa.Views;
+using DryIoc;
+using ReactiveUI;
+
+partial class App
+{
+	public void RegisterViews(IContainer container)
+	{
+		container.Register<EmotionViewModel>();
+container.Register<IViewFor<EmotionViewModel>,EmotionView>();
+container.Register<GenerateCompositionSetViewModel>();
+container.Register<IViewFor<GenerateCompositionSetViewModel>,GenerateCompositionSetView>();
+container.Register<HomeViewModel>();
+container.Register<IViewFor<HomeViewModel>,HomeView>();
+container.Register<SelectProjectDirectoryViewModel>();
+container.Register<IViewFor<SelectProjectDirectoryViewModel>,SelectProjectDirectoryView>();
+	}
+}
