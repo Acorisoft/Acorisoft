19aa4db1333d807fda7a2af4c0dcdabaddd6edc6
Acoris <731984870@qq.com>
2021-04-08 16:22:11 +0800

    保存更改

--- src/Acorisoft.Morisa.Documents/Morisa/IDataSetFactory.cs
+++ src/Acorisoft.Morisa.Documents/Morisa/IDataSetFactory.cs
@@ -11,19 +11,5 @@ namespace Acorisoft.Morisa
     /// </summary>
     public interface IDataSetFactory : IDisposable
     {
-        /// <summary>
-        /// 获取一个观测当前数据集工厂是否打开的数据流。
-        /// </summary>
-        IObservable<bool> IsOpenStream { get; }
-
-        /// <summary>
-        /// 获取一个观测当前数据集工厂资源存储完毕事件的数据流。
-        /// </summary>
-        IObservable<Resource> CompletedStream { get; }
-
-        /// <summary>
-        /// 获取一个观测当前数据集工厂引发异常的数据流。
-        /// </summary>
-        IObservable<Exception> ExceptionStream { get; }
     }
 }
