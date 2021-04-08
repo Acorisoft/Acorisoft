f335094072613774676b2a55196fa38257ce0e39
Acoris <731984870@qq.com>
2021-04-08 11:23:33 +0800

    保存更改

---
+++ src/Acorisoft.Morisa.Documents/Morisa/Core/Disposable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.Morisa.Core
+{
+    /// <summary>
+    /// <see cref="Disposable"/> 类型实例，用于为 <see cref="Morisa"/> 提供对象可释放支持。
+    /// </summary>
+    public abstract class Disposable : IDisposable
+    {
+        private bool DisposedValue;
+
+        protected virtual void OnDisposeUnmanagedCore()
+        {
+
+        }
+
+        protected virtual void OnDisposeManagedCore()
+        {
+
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!DisposedValue)
+            {
+                if (disposing)
+                {
+                    OnDisposeManagedCore();
+                }
+
+                OnDisposeUnmanagedCore();
+                DisposedValue = true;
+            }
+        }
+
+        // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        ~Disposable()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/Acorisoft.Morisa.Documents/Morisa/IDataSetFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.Morisa
+{
+    /// <summary>
+    /// <see cref="IDataSetFactory"/> 接口用于表示一个抽象的数据集工厂接口，用于为用户提供数据集操作支持。
+    /// </summary>
+    public interface IDataSetFactory : IDisposable
+    {
+        /// <summary>
+        /// 获取一个观测当前数据集工厂是否打开的数据流。
+        /// </summary>
+        IObservable<bool> IsOpenStream { get; }
+
+        /// <summary>
+        /// 获取一个观测当前数据集工厂资源存储完毕事件的数据流。
+        /// </summary>
+        IObservable<Resource> CompletedStream { get; }
+
+        /// <summary>
+        /// 获取一个观测当前数据集工厂引发异常的数据流。
+        /// </summary>
+        IObservable<Exception> ExceptionStream { get; }
+    }
+}
