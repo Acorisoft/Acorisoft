1ccdf521372da491794c060d82c388ed1b63c901
Acoris <731984870@qq.com>
2021-04-11 16:04:06 +0800

    保存更改

--- src/Acorisoft.Morisa.Shell/Properties/AssemblyInfo.cs
+++ src/Acorisoft.Morisa.Shell/Properties/AssemblyInfo.cs
@@ -1,3 +1,9 @@
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/Shell", "Acorisoft.Buttons")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/Shell", "Acorisoft.Dialogs")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/Shell", "Acorisoft.ViewModels")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/Shell", "Acorisoft.Views")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/Shell", "Acorisoft.Windows")]
---
+++ src/Acorisoft.Morisa.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI", "Acorisoft.Morisa")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI", "Acorisoft.Morisa.Converters")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI", "Acorisoft.Morisa.Converters.Xaml")]
