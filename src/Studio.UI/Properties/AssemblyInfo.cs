77c27f21a042cf9566e8addacddd704317029698
Acoris <731984870@qq.com>
2021-04-27 19:32:10 +0800

    remove all change

--- src/Studio.UI/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Windows;
-using System.Windows.Markup;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/Morisa/UI", "Acorisoft.Spa")]
--- src/Studio/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Windows;
-using System.Windows.Markup;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
