849d2bbf2283feceeedd6fe3c2254f5d28f8f292
Acoris <731984870@qq.com>
2021-05-28 17:56:30 +0800

    添加了索引器

--- src/Acorisoft.Studio.Documents/Documents/Document.cs
+++ src/Acorisoft.Studio.Documents/Documents/Document.cs
@@ -7,5 +7,15 @@ namespace Acorisoft.Studio.Documents
     {
         [BsonId]
         public Guid Id { get; set; }
+        
+        /// <summary>
+        /// 最后一次修改时间
+        /// </summary>
+        public DateTime LastAccessBy { get; set; }
+        
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedBy { get; set; }
     }
 }
